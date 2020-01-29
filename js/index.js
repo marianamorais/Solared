635850f4f78b66d13432b372249e21b47cbfdba5
Mariana <moraismariana83@gmail.com>
2020-01-29 17:56:07 -0300

    Nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
-  $('#icon').click(function(){
-    $('ul').toggleClass('active');
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+  const scrollPos = window.scrollY;
+  if(scrollPos > 10){
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+})
+
+window.addEventListener('click', () => {
+  const positionState = window.fixed;
+  
+  $('.menu-btn').click(function(e) {
+    e.preventDefault();
+    var positionState = 'sticky'
+
+    if (clicked == "false") {
+    positionState = 'fixed';
+    clicked = "true";
+    } else {
+    overflowState = 'sticky';
+    clicked = "false";
+    }
+    $('html, body').css('position', positionState);
   });
 });
